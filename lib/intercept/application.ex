df9cf0dc2b1e4cfb21845c6bb185098f198cebff
Rafael Belo <rafaelrpbelo@gmail.com>
2020-04-29 23:05:54 -0300

    Setup minimal Intercept.Application

--- lib/intercept/application.ex
+++ lib/intercept/application.ex
@@ -1,10 +1,16 @@
 defmodule Intercept.Application do
   use Application
+  require Logger
 
   def start(_type, _args) do
-    children = []
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: Intercept.Plug, options: [port: 5000]}
+    ]
 
     opts = [strategy: :one_for_one, name: Intercept.Supervisor]
+
+    Logger.info("Starting application...")
+
     Supervisor.start_link(children, opts)
   end
 end
