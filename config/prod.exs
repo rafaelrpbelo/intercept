80b7b5b07baaf0cf1268867647a18d56c640ddff
Rafael Belo <rafaelrpbelo@gmail.com>
2020-12-06 21:16:20 -0300

    Add config options for prod config file

--- config/prod.exs
+++ config/prod.exs
@@ -2,4 +2,7 @@ import Config
 
 config :intercept, Intercept.Mailer,
   adapter: Bamboo.SendGridAdapter,
-  api_key: System.get_env("SENDGRID_API_KEY")
+  api_key: System.get_env("SENDGRID_API_KEY"),
+  to: System.get_env("INTERCEPT_MAIL_TO"),
+  from: System.get_env("INTERCEPT_MAIL_FROM", "no-reply@intercept.default"),
+  subject_prefix: System.get_env("INTERCEPT_MAIL_SUBJECT_PREFIX", "[INTERCEPT]")
