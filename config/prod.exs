3dd223af4aa1ac050f3271256f157c52989caefc
Rafael Belo <rafaelrpbelo@gmail.com>
2021-05-16 22:48:49 -0300

    Fix config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,10 @@
 import Config
 
+config :intercept, Intercept.Email,
+  from: System.get_env("INTERCEPT_MAIL_FROM", "no-reply@intercept.default"),
+  to: System.get_env("INTERCEPT_MAIL_TO"),
+  subject_prefix: System.get_env("INTERCEPT_MAIL_SUBJECT_PREFIX", "[INTERCEPT]")
+
 config :intercept, Intercept.Mailer,
   adapter: Bamboo.SendGridAdapter,
-  api_key: System.get_env("SENDGRID_API_KEY"),
-  to: System.get_env("INTERCEPT_MAIL_TO"),
-  from: System.get_env("INTERCEPT_MAIL_FROM", "no-reply@intercept.default"),
-  subject_prefix: System.get_env("INTERCEPT_MAIL_SUBJECT_PREFIX", "[INTERCEPT]")
+  api_key: System.get_env("SENDGRID_API_KEY")
