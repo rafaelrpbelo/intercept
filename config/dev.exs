7115486953845a23a5cb798726a8a37905da8cd6
Rafael Belo <rafaelrpbelo@gmail.com>
2021-05-16 22:07:47 -0300

    Add default "from" and "to" on dev settings

--- config/dev.exs
+++ config/dev.exs
@@ -3,6 +3,10 @@ import Config
 config :intercept, Intercept.Mailer,
   adapter: Bamboo.LocalAdapter
 
+config :intercept, Intercept.Email,
+  from: "support@myapp.com",
+  to: "test@me.com"
+
 if System.get_env("SMTP_SERVER") do
   config :intercept, Intercept.Mailer,
     adapter: Bamboo.SMTPAdapter,
